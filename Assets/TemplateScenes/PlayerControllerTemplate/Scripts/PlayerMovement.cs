1c58c9876b4402e43dedba2d391d0044cd80eae6
MrRoteks <rotkol@wp.pl>
2023-04-23 19:25:07 +0200

    Initial bhop mechanic

--- Assets/TemplateScenes/PlayerControllerTemplate/Scripts/PlayerMovement.cs
+++ Assets/TemplateScenes/PlayerControllerTemplate/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -11,11 +13,17 @@ public class PlayerMovement : MonoBehaviour {
 	float horizontalMove = 0f;
 	bool jump = false;
 	bool crouch = false;
+	bool bhop_active = false;
+	public int bhop_c = 1;
 	
-	// Update is called once per frame
-	void Update () {
 
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    // Update is called once per frame
+    void Update () {
+        //--------- BHOP ---------//
+        bhop_c = controller.GetComponent<CharacterController2D>().bhop_count;
+		runSpeed = 40 * bhop_c;
+        //-----------------------//
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
 		if (Input.GetButtonDown("Jump"))
 		{
