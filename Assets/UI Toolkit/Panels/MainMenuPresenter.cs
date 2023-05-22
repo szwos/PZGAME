09a58f5a4a7bccba568ca4c9b330e0d0b2154262
szwos <szwos5@gmail.com>
2023-05-22 19:52:45 +0200

    Added switching scene

--- Assets/UI Toolkit/Panels/MainMenuPresenter.cs
+++ Assets/UI Toolkit/Panels/MainMenuPresenter.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
+
+    [SerializeField]
+    public string startingScene = "level0";
+    public string settingsScene = "settingsScene";
+
     private void Awake()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         root.Q<Button>("Start").clicked += () => Debug.Log("start button clicked");
+        root.Q<Button>("Start").clicked += () => SceneManager.LoadScene(startingScene);
+
         root.Q<Button>("Settings").clicked += () => Debug.Log("settings button clicked");
+
         root.Q<Button>("Quit").clicked += () => Debug.Log("quit button clicked");
     }
 }
