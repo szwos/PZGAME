d12b705f8113180323b683a9e8de70a1e5565e10
szwos <szwos5@gmail.com>
2023-06-03 20:23:17 +0200

    added flipping player

---
+++ Assets/Scripts/CharacterController/FlipCharacter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipCharacter : MonoBehaviour
+{
+    private float xInput;
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");
+
+        if(xInput > 0.1)
+        {
+            //gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        } else if (xInput < -0.1)
+        {
+            //gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+}
